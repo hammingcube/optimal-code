b5782fe5c21e805861adaf31e9d4c778ff25a759
maddyonline <madhavjha@gmail.com>
2016-11-11 22:11:32 -0800

    more

--- models/main.go
+++ models/main.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"github.com/maddyonline/umpire"
 )
 
@@ -8,10 +9,12 @@ type UserKey string
 type ProblemKey string
 
 type User struct {
-	Name string `json:"name"`
+	Id   UserKey `json:"id"`
+	Name string  `json:"name"`
 }
 
 type Problem struct {
+	Id        ProblemKey        `json:"id"`
 	Name      string            `json:"name"`
 	Title     string            `json:"title"`
 	ShortDesc string            `json:"short_desc"`
@@ -19,15 +22,52 @@ type Problem struct {
 	Templates map[string]string `json:"templates"`
 }
 
+type Solution struct {
+	ProblemId ProblemKey `json:"problem_id"`
+	*umpire.Payload
+}
+
 type Submission struct {
-	Problem   string `json:"problem"`
+	ProblemId string `json:"problem_id"`
 	Timestamp string `json:"timestamp"`
 	Solution  *umpire.Payload
 }
 
 type Schema struct {
 	Problems    map[ProblemKey]*Problem
-	Solutions   map[ProblemKey]*umpire.Payload
+	Solutions   map[ProblemKey]*Solution
 	Users       map[UserKey]*User
 	Submissions map[UserKey][]*Submission
+}
+
+type Store interface {
+	CreateProblem(key ProblemKey, p *Problem) (error, *Problem)
+	CreateSolution(key ProblemKey, s *Solution) (error, *Solution)
+}
+
+type InMemoryStore struct {
+	Problems  map[ProblemKey]*Problem
+	Solutions map[ProblemKey]*Solution
+}
+
+func (store *InMemoryStore) CreateProblem(key ProblemKey, p *Problem) (error, *Problem) {
+	if p == nil {
+		return nil, nil
+	}
+	if _, ok := store.Problems[key]; ok {
+		return fmt.Errorf("Key exists"), nil
+	}
+	store.Problems[key] = p
+	return nil, p
+}
+
+func (store *InMemoryStore) CreateSolution(key ProblemKey, s *Solution) (error, *Solution) {
+	if s == nil {
+		return nil, nil
+	}
+	if _, ok := store.Solutions[key]; ok {
+		return fmt.Errorf("Key exists"), nil
+	}
+	store.Solutions[key] = s
+	return nil, s
 }
--- models/main_test.go
+++ models/main_test.go
@@ -3,6 +3,7 @@ package models
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/maddyonline/umpire"
 	"io/ioutil"
 	"log"
 	"testing"
@@ -22,4 +23,46 @@ func TestDecoding(t *testing.T) {
 	fmt.Printf("%s %#v\n", "Solutions", v.Solutions)
 	fmt.Printf("%s %#v\n", "Users", v.Users)
 	fmt.Printf("%s %#v\n", "Submissions", v.Submissions)
+}
+
+func try_creating_problems(store Store, prob *Problem) error {
+	err, stored := store.CreateProblem(prob.Id, prob)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Stored: %#v", stored)
+	return nil
+}
+
+func try_creating_solutions(store Store, soln *Solution) error {
+	err, stored := store.CreateSolution(soln.ProblemId, soln)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Stored: %#v", stored)
+	return nil
+}
+
+func TestStoreImpl(t *testing.T) {
+	store := &InMemoryStore{
+		Problems:  map[ProblemKey]*Problem{},
+		Solutions: map[ProblemKey]*Solution{},
+	}
+	prob := &Problem{
+		Id:    "abc",
+		Name:  "prob-1",
+		Title: "Sort numbers in array",
+	}
+	soln := &Solution{
+		prob.Id,
+		&umpire.Payload{
+			Language: "cpp",
+			Stdin:    "",
+			Files: []*umpire.InMemoryFile{
+				&umpire.InMemoryFile{"abc.cpp", ""},
+			},
+		},
+	}
+	try_creating_problems(store, prob)
+	try_creating_solutions(store, soln)
 }
